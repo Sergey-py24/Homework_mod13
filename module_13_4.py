485dd694019aebaea458c64005cb7ef628228803
Sergey-py24 <chuvachoc2009@yandex.ru>
2024-11-19 22:30:55 +0300

    Add files via upload

---
+++ module_13_4.py
@@ -0,0 +1,53 @@
+
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+import asyncio
+
+api = ''
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+@dp.message_handler(text="Мои калории")
+async def set_age(message):
+    await message.answer(f'{message.from_user.first_name}, введите свой возраст.')
+    await UserState.age.set()
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message, state):
+    await state.update_data(age = message.text)
+    await message.answer(f'{message.from_user.first_name}, введите свой рост.')
+    await UserState.growth.set()
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message, state):
+    await state.update_data(growth = message.text)
+    await message.answer(f'{message.from_user.first_name}, введите свой вес.')
+    await UserState.weight.set()
+
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message, state):
+    await state.update_data(weight = message.text)
+    data = await state.get_data()
+    clr = int(10 * int(data['weight']) + 6.25 * int(data['growth']) - 5 * int(data['age']) + 5)
+    await message.answer(f'{message.from_user.first_name}, ваша норма калорий - {clr}.')
+    await state.finish()
+
+@dp.message_handler(commands=['start'])
+async def start_message(message):
+    await message.answer(f"Приветствуем вас,{message.from_user.first_name} {message.from_user.last_name}!")
+
+@dp.message_handler()
+async def all_messages(message):
+    await message.answer('Доброго времени суток!'
+                         'Введите команду /start, чтобы начать общение')
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
